7e83dd687b829da7c8a958b325b3e051b90fa42b
kaylafinkelstein <k.finkelstein4000@gmail.com>
2022-08-04 19:32:03 -0400

    KanyeRest

---
+++ KanyeREST/Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeREST
+{
+    public class Program
+    {
+      
+        public static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var quote = new QuoteGenerator(client);
+
+            Console.WriteLine("And now: a funny but accurate way to describe life with quotes from Kanye West and Ron Swanson");
+
+            Console.WriteLine("------------------------");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Kanye said: {quote.KanyeQuote()}");
+
+                Console.WriteLine("------------------------");
+
+                Console.WriteLine($"Ron said: {quote.RonQuote()}");
+
+                Console.WriteLine("------------------------");
+
+
+            }
+        }
+    }  
+}
